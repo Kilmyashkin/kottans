187c633866b7448f4bfc1812e6c82d55f5360430
Kilmiashkin <akilmyashkin@gmail.com>
2016-06-06 12:05:43 +0300

    Refactor deepAssign function

--- week03/index.js
+++ week03/index.js
@@ -5,8 +5,8 @@ function deepAssign(obj, ...rest) {
   while (rest.length) {
     let item = rest.shift();
     for (let key of Object.keys(item)) {
-      if (typeof obj[key] === 'object' && obj[key] !=null && !Object.is(obj[key], '[object Array]')) {
-        if (typeof item[key] === 'object' && item[key] != null) {
+      if (isObject(obj[key])) {
+        if (isObject(item[key])) {
           obj[key] = deepAssign(obj[key], item[key]);
         } else {
           obj[key] = item[key];
@@ -17,4 +17,8 @@ function deepAssign(obj, ...rest) {
     }
   }
   return obj;
+}
+
+function isObject(value) {
+  return value != null && typeof value === 'object'
 }
